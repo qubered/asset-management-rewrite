3a753021fb4c3a6f4ee0dd59e5ef3032c520337b
Jayden Nawotka <156985453+qubered@users.noreply.github.com>
2025-03-09 22:17:02 +1100

    add indent to child links in sidebar

--- src/components/navbar/app-sidebar.tsx
+++ src/components/navbar/app-sidebar.tsx
@@ -51,8 +51,8 @@ const data = {
 }
 
 export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
-  const { data: session } = authClient.useSession() 
-  const userData: {fullName?: string, email?: string, avatar?: string} = {fullName: session?.user.name, email: session?.user.email}
+  const { data: session } = authClient.useSession()
+  const userData: { fullName?: string, email?: string, avatar?: string } = { fullName: session?.user.name, email: session?.user.email }
   const currentPath = usePathname()
   return (
     <Sidebar {...props}>
@@ -87,7 +87,7 @@ export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
                     {item.items.map((item) => (
                       <SidebarMenuItem key={item.title}>
                         <SidebarMenuButton asChild isActive={item.url === currentPath}>
-                          <a href={item.url}>{item.title}</a>
+                          <a href={item.url} className="ml-2">{item.title}</a>
                         </SidebarMenuButton>
                       </SidebarMenuItem>
                     ))}
