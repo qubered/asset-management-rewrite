179b15f3ab31ac6551cb34396109ea2ae52f520f
Jayden Nawotka <156985453+qubered@users.noreply.github.com>
2025-03-10 16:42:10 +1100

    Add logic for password reset

--- src/app/auth/resetpassword/page.tsx
+++ src/app/auth/resetpassword/page.tsx
@@ -21,24 +21,24 @@ import { z } from "zod"
 import { zodResolver } from "@hookform/resolvers/zod"
 import { useForm } from "react-hook-form"
 import { authClient } from "@/lib/auth-client";
-import { useState } from "react"
+import { useState, useEffect } from "react"
 import { MoonLoader } from "react-spinners"
 import { AlertBox } from "@/components/alert-helper"
+import { useSearchParams } from "next/navigation"
 import Link from "next/link";
-import { toast } from "sonner"
 
 const formScheme = z.object({
-    email: z.string().email(),
-
+    password: z.string().min(8),
 })
 
 
-
-export default function ForgotPasswordForm() {
+export default function ResetPasswordForm() {
+    const searchParams = useSearchParams();
+    const [token, setToken] = useState<string | null>(null);
     const form = useForm<z.infer<typeof formScheme>>({
         resolver: zodResolver(formScheme),
         defaultValues: {
-            email: "",
+            password: "",
         }
     });
     const [isLoading, setIsLoading] = useState(false);
@@ -47,13 +47,17 @@ export default function ForgotPasswordForm() {
         title: "",
         description: "",
         variant: "default" as "default" | "success" | "error",
-    })
+    });
+
+    useEffect(() => {
+        setToken(searchParams.get("token"));
+    }, [searchParams]);
 
     async function onSubmit(values: z.infer<typeof formScheme>) {
         setAlert({ show: false, title: "", description: "", variant: "default" })
-        const { data, error } = await authClient.forgetPassword({
-            email: values.email,
-            redirectTo: "/auth/resetpassword"
+        const { data, error } = await authClient.resetPassword({
+            newPassword: values.password,
+            token: token || ""
         }, {
             onRequest: (ctx) => {
                 setIsLoading(true)
@@ -81,23 +85,38 @@ export default function ForgotPasswordForm() {
         });
     }
 
+
+    if (!token) {
+        return (
+            <div className="flex flex-col items-center gap-8">
+                <AlertBox
+                    show={true}
+                    title="Error"
+                    description="No token found in URL. Did you follow the link from the email?"
+                    variant="error"
+                />
+                <Link href="/auth/signin"><Button>Return to Sign In</Button></Link>
+            </div>
+        );
+    }
+
     return (
         <div className={cn("flex flex-col gap-6 w-120")}>
             <AlertBox show={alert.show} title={alert.title} description={alert.description} variant={alert.variant} />
             <Card>
 
                 <CardHeader>
-                    <CardTitle>Request a password reset</CardTitle>
+                    <CardTitle>Reset Your Password</CardTitle>
                 </CardHeader>
                 <CardContent>
                     <Form {...form}>
                         <form onSubmit={form.handleSubmit(onSubmit)} className="flex flex-col gap-8">
                             <FormField
                                 control={form.control}
-                                name="email"
+                                name="password"
                                 render={({ field }) => (
                                     <FormItem>
-                                        <FormLabel>Email</FormLabel>
+                                        <FormLabel>New Password</FormLabel>
                                         <FormControl>
                                             <Input placeholder="john@doe.com" {...field} />
                                         </FormControl>
@@ -107,7 +126,7 @@ export default function ForgotPasswordForm() {
                             <Button type="submit" disabled={isLoading}>
                                 <div className="flex gap-4 items-center">
                                     {isLoading ? (<MoonLoader size={20} />) : (null)}
-                                    Send Request
+                                    Reset Password
                                 </div>
 
                             </Button>
