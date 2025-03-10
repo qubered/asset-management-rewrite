acebbd84ec6191b03e120b0d4d031b408433de74
Jayden Nawotka <156985453+qubered@users.noreply.github.com>
2025-03-10 15:26:38 +1100

    Add theme toggle to auth layout

--- src/app/auth/layout.tsx
+++ src/app/auth/layout.tsx
@@ -1,13 +1,19 @@
+import { ModeToggle } from "@/components/themes/theme-button";
 export default function AuthLayout({
-    children,
-  }: Readonly<{
-    children: React.ReactNode;
-  }>) {
-    return (
-        <div className="flex flex-col justify-center items-center h-screen gap-2">
-            <div>
-                {children}
-            </div>
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <>
+      <header className="sticky top-0 flex justify-end h-16 shrink-0 items-center gap-2 px-4">
+        <ModeToggle />
+      </header>
+      <div className="flex flex-col justify-center items-center h-screen gap-2">
+        <div>
+          {children}
         </div>
-    )
+      </div>
+    </>
+  )
 }
