62f1f45dfd8d06d8f995985ef22dc241c00e1e8a
Jayden Nawotka <156985453+qubered@users.noreply.github.com>
2025-03-10 19:13:11 +1100

    change auth-client to using env variable

--- src/lib/auth-client.ts
+++ src/lib/auth-client.ts
@@ -1,9 +1,10 @@
 import { createAuthClient } from "better-auth/react"
 import { organizationClient } from "better-auth/client/plugins"
+import { env } from "@/env"
 
 export const authClient = createAuthClient({
-    baseURL: "http://localhost:3000", // the base url of your auth server 
-    plugins: [ 
-        organizationClient() 
-    ] 
+    baseURL: env.BETTER_AUTH_URL, // the base url of your auth server 
+    plugins: [
+        organizationClient()
+    ]
 })
